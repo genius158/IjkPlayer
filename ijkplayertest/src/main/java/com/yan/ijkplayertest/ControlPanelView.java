e8d0e65892297715925c9b61ad52a0b97c637ccb
yan <yanxianwei1114@gmail.com>
2017-12-29 17:47:44 +0800

    adjust urls

--- ijkplayertest/src/main/java/com/yan/ijkplayertest/ControlPanelView.java
+++ ijkplayertest/src/main/java/com/yan/ijkplayertest/ControlPanelView.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
